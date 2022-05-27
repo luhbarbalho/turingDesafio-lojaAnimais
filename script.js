bf1211dce396ea188650d718857bf20e8153c023
luhbarbalho <luh.barbalho@gmail.com>
2022-05-27 04:02:32 -0300

    :package: blog + fotos + footer

---
+++ script.js
@@ -0,0 +1,23 @@
+//! CÓDIGO PARA A ANIMAÇÃO DAS FOTOS (SUBINDO NO SCROLL)
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("scroll", function(event) {
+        const animatedFotos = document.getElementsByClassName("fotos");
+        const windowOffsetTop = window.innerHeight + window.scrollY;
+
+        Array.prototype.forEach.call(animatedFotos, (animatedBox) => {
+            const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+            if (windowOffsetTop >= animatedBoxOffsetTop) {
+                addClass(animatedBox, "fade-in");
+            }
+        });
+    });
+});
+
+function addClass(element, className) {
+    const arrayClasses = element.className.split(" ");
+    if (arrayClasses.indexOf(className) === -1) {
+        element.className += " " + className;
+    }
+}
